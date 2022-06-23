4a5663bad92bb4075ae547a8f7cc1dbc946abd35
Shaffat-BS23 <Shaffat.Nur@brainstation-23.com>
2022-06-23 20:45:56 +0600

    modify settings base

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--gdf!kz+(f$+ug3l=%fdiqe0%6$od4v_d6k93!xoeznt*^*g-@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
